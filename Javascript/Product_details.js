cd7a3456d18dcb92e8288c76fb1cc224dd24a3ca
nguyendinhchuong <chuongnguyen.25203@gmail.com>
2023-11-26 22:13:04 +0700

    details_function

---
+++ Javascript/Product_details.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+    //heart
+    $("#lovev").on("click", function () {
+        $(this).html('<i class="fa-solid fa-heart fa-xl pr-3" style="color: #c00202;"></i>');
+    });
+    var currentState = 1;
+
+    $("#lovev").on("click", function () {
+        if (currentState === 1) {
+            $(this).html('<i class="fa-solid fa-heart fa-xl pr-3" style="color: #c00202;"></i>');
+            currentState = 2;
+        } else {
+            $(this).html('<i class="fa-regular fa-heart fa-beat fa-xl pr-3" style="color: #ff424f;"></i>');
+            currentState = 1;
+        }
+    });
+
+    //thumb
+    var currentImagePath = "";
+    $(".small_thumb").on({
+        click: function () {
+            var imagePath = $(this).attr("src");
+
+            if (currentImagePath !== imagePath) {
+                $("#thumb").html('<img class="item_thumb" src="' + imagePath + '">');
+                currentImagePath = imagePath;
+            }
+        },
+        mouseenter: function () {
+            var imagePath = $(this).attr("src");
+
+            if (currentImagePath !== imagePath) {
+                $("#thumb").html('<img class="item_thumb" src="' + imagePath + '">');
+                currentImagePath = imagePath;
+            }
+        }
+
+    });
+
+});
+
